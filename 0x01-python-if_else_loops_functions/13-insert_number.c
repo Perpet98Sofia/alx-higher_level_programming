c3c3688cb4073d9844c02e26ed85d3b56b3bbf22
Perpet98Sofia <perpetualakwiri@gmail.com>
2023-10-03 15:47:43 -0700

    task 13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,44 +1,70 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * insert_node - inserts a node
- * @head: linked list to inster into
- * @number: value of node to insert
- * Return: address of the new node
+ * insert_node - malloc and insert node into sorted singly linked list
+ * @head: pointer to head of linked list
+ * @number: data for new node
+ * Return: address of new node, or NULL if failed
  */
+
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *p1 = *head, *p2, *new = malloc(sizeof(listint_t));
+	listint_t *tmp = NULL;
+	listint_t *new = NULL;
 
+	if (!head)
+		return (NULL);
+
+	/* malloc new node */
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 	new->n = number;
 	new->next = NULL;
 
-	if (p1 == NULL)
+	/* if no linked list, insert node as the only member */
+	if (*head == NULL)
 	{
 		*head = new;
+		(*head)->next = NULL;
+		return (new);
+	}
+	/* if only one node in linked list, do comparision and insert */
+	if ((*head)->next == NULL)
+	{
+		if ((*head)->n < new->n)
+			(*head)->next = new;
+		else
+		{
+			new->next = *head;
+			*head = new;
+		}
 		return (new);
 	}
 
-	if (p1->n > number)
+	/* if lots of nodes in linked list, do comparision and insert */
+	tmp = *head;
+	while (tmp->next != NULL)
 	{
-		*head = new;
-		new->next = p1;
-		return (new);
+		/* if new node num is smaller than first node, insert */
+		if (new->n < tmp->n)
+		{
+			new->next = tmp;
+			*head = new;
+			return (new);
+		}
+		/* if new node num is the same as an existing node, insert */
+		/* compare previous node and next node, insert in between */
+		if (((new->n > tmp->n) && (new->n < (tmp->next)->n)) ||
+		    (new->n == tmp->n))
+		{
+			new->next = tmp->next;
+			tmp->next = new;
+			return (new);
+		}
+		tmp = tmp->next;
 	}
-
-	p2 = p1->next;
-
-	while (p2 != NULL)
-	{
-		if (p2->n > number)
-			break;
-		p1 = p2;
-		p2 = p2->next;
-	}
-
-	p1->next = new;
-	new->next = p2;
-
+	/* if new node is greatest and never inserted, insert now */
+	tmp->next = new;
 	return (new);
 }

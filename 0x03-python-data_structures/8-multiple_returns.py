f9daa95b42a4bae48ee553067148c8ef2d2a40e1
Perpet98Sofia <perpetualakwiri@gmail.com>
2023-10-09 15:26:52 -0700

    rest

--- 0x03-python-data_structures/8-multiple_returns.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def multiple_returns(sentence):
-    if not sentence:
-        length = 0
-        first = None
-    else:
-        length = len(sentence)
-        first = sentence[0]
-    return(length, first)

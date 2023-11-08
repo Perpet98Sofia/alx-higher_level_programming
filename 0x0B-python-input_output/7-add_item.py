e6fd9ad6ba568c58982c713c331fd12ea14072b3
Perpet98Sofia <perpetualakwiri@gmail.com>
2023-11-08 19:59:08 +0000

    advanced_input

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
-"""Module 7-save_to_json_file.
-Writes an Object to a text file,
-using a JSON representation.
-"""
+""" Module """
+import sys
+from os import path
 
 
-import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
-def save_to_json_file(my_obj, filename):
-    """Writes the representation of my_obj
-    to filename.
-    Args:
-        - my_obj: object to write
-        - filename: file to write into
-    """
-
-    with open(filename, 'w+') as f:
-        json.dump(my_obj, f)
+new_list = []
+if path.isfile("add_item.json"):
+    new_list = load_from_json_file("add_item.json")
+save_to_json_file(new_list + sys.argv[1:], "add_item.json")
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-"""student class module"""
+"""initializate"""
 
 
 class Student:
-    """a class we shall json"""
+    """create class student"""
 
     def __init__(self, first_name, last_name, age):
-        """init method"""
+        """initializate atrributes
+        :param first_name:
+        :param last_name:
+        :param age:
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-        """get dict, names if only string"""
-        if type(attrs) is list and all([type(a) == str for a in attrs]):
-            return {name: value for name, value in self.__dict__.items()
-                    if name in attrs}
-        else:
-            return self.__dict__.copy()
-
-    def reload_from_json(self, json):
-        '''gets dict attr from json.'''
-        for key, value in json.items():
+    def to_json(self):
+        """ retrieves a dictionary """
+        return self.__dict__

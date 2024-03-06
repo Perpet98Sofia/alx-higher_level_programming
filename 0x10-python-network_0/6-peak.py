ac8d7a2f7ca39c048c0ce42da7e2143517713f01
Perpet98Sofia <perpetualakwiri@gmail.com>
2024-03-06 07:08:17 -0800

    network_0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-"""Find a Peak in a List"""
-def find_peak(list_of_integers):
-    """Find a Peak in a list
+"""Write a function that finds a peak in a list of unsorted integers."""
 
-    Args:
-        list_of_integers: list of int
-    Returns:
-        the value or None
-    """
-    lis = list_of_integers
-    b = len(lis)
-    if (b <= 2):
-        return (None)
-    for i in range(1, b - 1):
-	    if (lis[i - 1] <= lis[i] >= lis[i + 1]):
-	        return (lis[i])
-    return (None)
+
+def find_peak(list_of_integers):
+    """[summary]"""
+    if list_of_integers:
+        list_of_integers.sort()
+        return list_of_integers[-1]
+    return None
